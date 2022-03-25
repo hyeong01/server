4074c1760325729f9f1c3287339bac00d2442e8e
hyeong <mnihy3275@naver.com>
2022-03-25 19:01:36 +0900

    combining js to html through express

--- app/app.js
+++ app/app.js
@@ -11,5 +11,6 @@ const home = require("./src/routes/home")
 app.set('views', './src/views');
 app.set('view engine', 'ejs')
 app.use("/", home); // use -> 미들 웨어 등록 메서드
+app.use(express.static(`${__dirname}/src/public`))
 
 module.exports = app;
